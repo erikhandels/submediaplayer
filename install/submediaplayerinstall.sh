e69c7a27316291410ea4c7b13a267cba7024538d
erikhandels <erikhandels@hotmail.com>
2024-04-11 15:42:09 +0200

    added install file

---
+++ install/submediaplayerinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Check if nodejs is installed"
+dpkg -s nodejs 2>/dev/null >/dev/null || curl -sL https://deb.nodesource.com/setup_5.x | sudo bash - | apt-get install >git clone https://github.com/erikhandels/submediaplayer.git
+
+echo "Installing submediaplayer in $PWD/submediaplayer"
+cd submediaplayer
+npm install
+echo "Install complete"
+
+sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart > /dev/null <<EOT
+@node $PWD/submediaplayer/server.mjs
+@xscreensaver -no-splash
+@xset s off
+@xset -dpms
+@xset s noblank
+@chromium-browser --kiosk http://localhost:3000
+EOT
+
+echo "Edited autostart file"
+echo ""
+echo -e "\033[32mPlease reboot the system to start submediaplayer\033[m"
+sudo reboot now > /dev/null
