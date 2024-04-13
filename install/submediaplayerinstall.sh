9ba4463a4681792bc6c26000fd3f5277dcfcb1e4
erikhandels <erikhandels@hotmail.com>
2024-04-13 18:15:24 +0200

    added delays

--- install/submediaplayerinstall.sh
+++ install/submediaplayerinstall.sh
@@ -2,6 +2,7 @@
 echo "Check if nodejs is installed"
 dpkg -s nodejs 2>/dev/null >/dev/null || curl -sL https://deb.nodesource.com/setup_5.x | sudo bash - | apt-get install nodejs
 rm -rf submediaplayer
+sleep 5s
 git clone https://github.com/erikhandels/submediaplayer.git
 
 echo "Installing submediaplayer in $PWD/submediaplayer"
@@ -9,6 +10,7 @@ cd submediaplayer
 npm install
 
 sudo sed -i "/#submediaplayer-start/,/#submediaplayer-end/d" /etc/xdg/lxsession/LXDE-pi/autostart
+sleep 2s
 sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart > /dev/null <<EOT
 
 #submediaplayer-start
@@ -25,4 +27,5 @@ EOT
 echo "Edited autostart file"
 echo -e "\033[32mInstall complete\033[m"
 echo "Attempting to reboot..."
+sleep 3s
 sudo reboot now > /dev/null
