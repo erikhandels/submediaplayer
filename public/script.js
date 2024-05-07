75e12ad98b9ea8fcdc304164d6839e008ab969a7
erikhandels <erikhandels@hotmail.com>
2024-05-08 00:07:57 +0200

    Added draggable hotspots and save/escape button

--- public/script.js
+++ public/script.js
@@ -13,12 +13,117 @@ const setup = item => {
   newVideo.id = item.video.id;
   newVideo.appendChild(newSource);
 
-  const newButton = buttonTemplate.cloneNode();
+  const newButton = buttonTemplate.cloneNode(true);
   newButton.id = item.button.id;
   Object.assign(newButton.style, item.button.style);
+  
+  const saveButton = document.getElementById('saveButton')
+  const escapeButton = document.getElementById('escapeButton')
+
   if(window.location.search.includes('debug')){
     newButton.classList.add('debug')
-    newButton.innerHTML = `<div class="debugName">${item.button.id}</div>`;
+    newButton.querySelector('.debugName').innerHTML = item.button.id;
+    
+    const resizeElement = newButton.querySelector('.debugResize');
+    const dragElement = newButton.querySelector('.dragArea')
+    let startX;
+    let startY;
+    let startWidth;
+    let startHeight;
+    let touchOffsetX;
+    let touchOffsetY;
+
+    resizeElement.addEventListener('touchstart', function(event) {
+        startX = event.touches[0].clientX;
+        startY = event.touches[0].clientY;
+        startWidth = parseInt(document.defaultView.getComputedStyle(newButton).width, 10);
+        startHeight = parseInt(document.defaultView.getComputedStyle(newButton).height, 10);
+    })
+
+    resizeElement.addEventListener('touchmove', function(event) {
+        var touch = event.touches[0];
+        var deltaX = touch.clientX - startX;
+        var newWidth = startWidth + deltaX;
+
+        var deltaY = touch.clientY - startY;
+        var newHeight = startHeight + deltaY;
+
+        // Set a minimum width to prevent the div from becoming too small
+        if (newWidth > 50) {
+          newButton.style.width = newWidth + 'px';
+        }
+        if (newHeight > 50) {
+          newButton.style.height = newHeight + 'px';
+      }
+    });
+
+    resizeElement.addEventListener('touchend', function(event) {
+        startWidth = parseInt(document.defaultView.getComputedStyle(newButton).width, 10);
+        startHeight = parseInt(document.defaultView.getComputedStyle(newButton).height, 10);
+    });
+
+    dragElement.addEventListener('touchstart', function(event) {
+      // Get the touch position relative to the element
+      var touch = event.touches[0];
+      touchOffsetX = touch.clientX - newButton.getBoundingClientRect().left;
+      touchOffsetY = touch.clientY - newButton.getBoundingClientRect().top;
+    });
+
+    dragElement.addEventListener('touchmove', function(event) {
+        // Prevent scrolling
+        event.preventDefault();
+
+        var touch = event.touches[0];
+        var newX = touch.clientX - touchOffsetX;
+        var newY = touch.clientY - touchOffsetY;
+
+        // Ensure the element stays within the bounds of the viewport
+        newX = Math.max(0, Math.min(window.innerWidth - newButton.offsetWidth, newX));
+        newY = Math.max(0, Math.min(window.innerHeight - newButton.offsetHeight, newY));
+
+        newButton.style.left = newX + 'px';
+        newButton.style.top = newY + 'px';
+    });
+
+    saveButton.addEventListener('touchstart', function(event) {
+      
+      console.log('save positions to JSON')
+      let style = document.defaultView.getComputedStyle(newButton)
+      let updatedValues = JSON.stringify({ "left": style.left, "right": style.top, "width": style.width, "height": style.height} )
+      console.log(updatedValues)
+
+      const data = { key: 'value' };
+
+      fetch('/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: updatedValues
+      })
+      .then(response => response.json())
+      .then(response => console.log(response.status))
+      .catch(error => {
+        // Handle error
+      });
+
+      setTimeout(() => {
+        // window.location.href = window.location.origin
+      }, 1000);
+    })
+
+    escapeButton.addEventListener('touchstart', function(event) {
+      window.location.href = window.location.origin
+    })
+
+  }else{
+    newButton.innerHTML = '';
+    if(saveButton){
+      saveButton.remove()
+    }
+    if(escapeButton){
+      escapeButton.remove()
+    }
   }
 
   videoContainer.append(newVideo);
@@ -45,4 +150,3 @@ fetch('./config.json')
     videoContainer.getElementsByTagName('video')[0].style.opacity = 1;
     videoContainer.getElementsByTagName('video')[0].play();
   })
-
